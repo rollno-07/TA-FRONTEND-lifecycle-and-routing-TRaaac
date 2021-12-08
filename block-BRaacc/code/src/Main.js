49794e18ff0e379d4150cb8b2afce3eb83e0306b
Vivek Rawat <vivekrawat0107@gmail.com>
2021-12-09 00:24:19 +0530

    updated

--- block-BRaacc/code/src/Main.js
+++ block-BRaacc/code/src/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import People from "./People";
+import Book from "./Book";
+import Article from "./Article";
+import App from "./App";
 
 function Main() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <Routes>
+        <Route path="/people" element={<People />} />
+        <Route path="/books" element={<Book />} />
+        <Route path="/articles" element={<Article />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Main;
--- block-BRaacc/code/src/Sidebar.js
+++ block-BRaacc/code/src/Sidebar.js
@@ -5,28 +5,39 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="icons">
-        <NavLink to="/" exact>
+        <NavLink to="/" exact activeC>
           <div className="icon">
             <i class="fas fa-home"> </i>
             <span> Home</span>
           </div>
         </NavLink>
-        <div className="icon">
-          <i class="fas fa-font"> </i>
-          <span> Articles</span>
-        </div>
-        <div className="icon">
-          <i class="fas fa-user-friends"> </i>
-          <span> People</span>
-        </div>
-        <div className="icon">
-          <i class="fas fa-book"> </i>
-          <span> Books</span>
-        </div>
-        <div className="icon">
-          <i class="fas fa-question-circle"> </i>
-          <span> Help & Supports</span>
-        </div>
+        <NavLink to="/articles" exact>
+          <div className="icon">
+            <i class="fas fa-font"> </i>
+            <span> Articles</span>
+          </div>
+        </NavLink>
+
+        <NavLink to="/people">
+          <div className="icon">
+            <i class="fas fa-user-friends"> </i>
+            <span> People</span>
+          </div>
+        </NavLink>
+
+        <NavLink to="/books" exact>
+          <div className="icon">
+            <i class="fas fa-book"> </i>
+            <span> Books</span>
+          </div>
+        </NavLink>
+
+        <NavLink to="/help&support" exact>
+          <div className="icon">
+            <i class="fas fa-question-circle"></i>
+            <span> Help & Supports</span>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
